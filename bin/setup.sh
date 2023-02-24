c52159bcde06b4f4caf7d5410b5559a357f77998
Anmol Chawla <anmolchawla10@gmail.com>
2023-02-24 17:48:14 +0530

    Add sd-1.5 download

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-mkdir -p "/opt/dashtoon/stable-diffusion-webui/models"
+mkdir -p "/opt/dashtoon/stable-diffusion-webui/models/Stable-diffusion"
 mkdir -p "/opt/dashtoon/stable-diffusion-webui/extensions"
 mkdir -p "/opt/dashtoon/stable-diffusion-webui/embeddings"
 mkdir -p "/opt/dashtoon/stable-diffusion-webui/outputs"
 
 cp ../docker-compose.yaml ../../docker-compose.yaml
+
+cd /opt/dashtoon/stable-diffusion-webui/models/Stable-diffusion/
+wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.ckpt
