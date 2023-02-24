7852796b7348d42b090f1cabb21a8a3ed9cc61a9
anmol <anmol@dashtoon.com>
2023-02-24 17:05:36 +0530

    Add a setup script

---
+++ bin/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p "/opt/dashtoon/stable-diffusion-webui/models"
+mkdir -p "/opt/dashtoon/stable-diffusion-webui/extensions"
+mkdir -p "/opt/dashtoon/stable-diffusion-webui/embeddings"
+mkdir -p "/opt/dashtoon/stable-diffusion-webui/outputs"
+
+cp ../docker-compose.yaml ../../docker-compose.yaml
